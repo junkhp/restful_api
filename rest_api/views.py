ebff63f621b35508933338bb0930b4dc35492dbb
Oshiba Junki <oshiba@m.cs.osakafu-u.ac.jp>
2020-12-06 22:39:29 +0900

    fix

--- rest_api/views.py
+++ rest_api/views.py
@@ -8,7 +8,9 @@ from .serializer import RecipesSerializer
 
 # Create your views here.
 class RecipesViewSet(viewsets.ModelViewSet):
+    # queryset = {'recipes': RecipesModel.objects.all()}
     queryset = RecipesModel.objects.all()
-    print(queryset)
+
     serializer_class = RecipesSerializer
+    # queryset = {'recipes': queryset}
     print(serializer_class)
