c25026bb8a005d7a2c2d5ff89a97f9fbf726a17b
Oshiba Junki <oshiba@m.cs.osakafu-u.ac.jp>
2020-12-09 02:38:40 +0900

    テストコマンドを書いたテキストファイルを追加

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -4,5 +4,5 @@ from rest_framework import routers
 from .views import RecipesViewSet
 
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'recipes', RecipesViewSet)
